1fca71578d79cf245efb65bcd004161fa0a31ba2
Francis Tchatchoua <f.tchatchoua@xlntelecom.co.uk>
2014-11-12 11:42:05 +0000

    angular basic

---
+++ AngularJsLearning/AngularJsLearning.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AngularJsLearning.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("XLN")]
+[assembly: AssemblyProduct("AngularJsLearning.Tests")]
+[assembly: AssemblyCopyright("Copyright © XLN 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f978b0df-63b0-4e17-944c-c643e3a11740")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AngularJsLearning/AngularJsLearning/Scripts/Custom/App.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="../jquery-2.1.1-vsdoc.js" />
+/// <reference path="angular.js" />
+(function () {
+  var gems = [
+    { name: 'Azurite',
+      price: 2,
+      description: 'Azurite solution to your problem',
+      canPurchase: true,
+      soldOut: false,
+      image: '/Images/custom/speaker109.png'
+    },
+    {
+      name: 'PLQuite',
+      price: 5.95,
+      description:'PLQuite solution to your problem',
+      canPurchase: true,
+      soldOut: false,
+      image: '/Images/custom/alarm48.png'
+    }
+  ];
+  
+  //create a module that will encapsulate the application data
+  var app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function () {
+    this.products = gems;
+  });
+
+})();
+
+
+/*
+
+NB: 
+
+-To format data we can use FILTERS
+-in order to 
+ 
+*/
+
+  
+
+
